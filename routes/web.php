7bd4fac8672429c29f5538fa6bda8b1e7fc4d809
OscarBecerril <oskbrrlz@gmail.com>
2021-02-26 21:34:38 -0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/clase', function () {
+    return view('mi-clase');
+});
+
+// Path de la ruta, 'NombreDelController@FunciónDentroDelController'
+Route::get('/prueba-controller', 'PruebaController@index');
+
+Route::resource('coins', 'CoinsController');
+
+Route::get('register', 'AuthController@register')->name('auth.register');
+Route::post('register', 'AuthController@doRegister')->name('auth.do-register');
+Route::get('login', 'AuthController@login')->name('auth.login');
+Route::post('login', 'AuthController@doLogin')->name('auth.do-login');
+Route::any('logout', 'AuthController@logout')->name('auth.logout');
